9f53512d4c9d70709f39e2a59ae25cad42f315e6
Rifqi ryandi <42662867+rifqiryandi@users.noreply.github.com>
2018-10-12 10:15:09 +0700

    Add files via upload
    Ini kiriman ulang saya

--- edit.php
+++ edit.php
@@ -20,12 +20,12 @@ include 'koneksi.php';
 		<input type="radio" name="rad" value="D3MI-41-02" <?php echo ($act['kelas']=='D3MI-41-02')?'checked':'' ?>>D3MI-41-02
 		<input type="radio" name="rad" value="D3MI-41-03" <?php echo ($act['kelas']=='D3MI-41-03')?'checked':'' ?>>D3MI-41-03 <br>
 	Jeniskelamin:<input type="radio" name="jk" value="laki-laki" <?php echo ($act['jeniskelamin']=='laki-laki')?'checked':'' ?>>Laki-laki<input type="radio" name="jk" value="Perempuan" <?php echo ($act['jeniskelamin']=='Perempuan')?'checked':'' ?>>Perempuan <br>
-	Hobi:<input type="checkbox" name="hb[]" value="Bola" >Bola
-		<input type="checkbox" name="hb[]" value="Musik">Musik
-		<input type="checkbox" name="hb[]" value="Catur">Catur <br>
+	Hobi:<input type="checkbox" name="hb[]" value="Bola" <?php if(in_array('Bola', $array)){echo "checked=checked";}?>>Bola
+		<input type="checkbox" name="hb[]" value="Musik" <?php if(in_array('Musik', $array)){echo "checked=checked";}?>>Musik
+		<input type="checkbox" name="hb[]" value="Catur"<?php if(in_array('Catur', $array)){echo "checked=checked";}?>>Catur <br>
 	Faklutas: <select name="ck1">
 		<option>---Pilih---</option>
-		<option value="FIT">FIT</option>
+		<option value="FIT">FIT</option> 
 		<option value="FIK">FIK</option>
 		<option value="FTE">FTE</option>
 	</select>	<br>
@@ -36,6 +36,29 @@ include 'koneksi.php';
 </form>
 
 <?php 
+if (isset($_POST['send'])) {
+		if (str_word_count($_POST['nama'])<=35) {
+			$nama = $_POST['nama'];
+		}else{echo "Nama terlalu panjang <br>";}
+
+		// if (!is_numeric($_POST['nim'])&&str_word_count($_POST['nim']>10)) {
+		// 	echo "NIM harus angka dan max 10 karakter <br>";
+		// }else{$nim = $_POST['nim'];}
+
+		$kelas = $_POST['rad'];
+		$jenisk = $_POST['jk'];
+
+		$jelas = $_POST['hb'];
+		$hobi = implode(",",$jelas);
+		$fakultas = $_POST['ck1'];
+		$Alamat = $_POST['alamat'];
+
+		$upd = mysqli_query($conn,"UPDATE datadiri SET nama='$nama',kelas='$kelas',jeniskelamin='$jenisk',Hobi='$hobi',Fakultas='$fakultas',Alamat='$Alamat' WHERE nim='$nim'");
+		if (isset($upd)) {
+			echo "berhasil";
+			header("Location:edit.php");
+		}
+		}
 // if (isset($_POST['send']){
 // 	$upd = mysqli_query($conn,"UPDATE `datadiri` SET `nama`='$_POST['nama']',`kelas`='$_POST['rad']',`jeniskelamin`='$_POST['jk']',`Hobi`='$_POST['hb']',`Fakultas`='$_POST['ck1']',`Alamat`='$_POST['alamat']' WHERE nim='$nim'");
 // }
